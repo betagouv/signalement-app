25ed7937c7b94680d3c08caaaf5dfe1fcc72e4ca
jrivals <jerome.rivals@gmail.com>
2018-11-14 09:24:50 +0100

    Suppression de la redirection et résolution de la variable d'environnement contenant le path de l'API avec subenv

--- server.js
+++ server.js
@@ -8,14 +8,6 @@ const app = server();
 // Serve only the static files form the dist directory
 app.use(server.static(__dirname + '/dist/signalement-app'));
 
-app.all('/api/*', (req, res) => {
-  res.redirect(307, process.env.API_BASE_URL + req.url);
-  request({ url: process.env.API_BASE_URL + req.url, headers: req.headers, body: req.body }, function(err, remoteResponse, remoteBody) {
-    if (err) { return res.status(500).end('Error'); }
-    res.end(remoteBody);
-  });
-});
-
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/signalement-app/index.html'));
 });
