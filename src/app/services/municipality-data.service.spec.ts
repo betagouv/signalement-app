936f4ac8313f321a80f298e01a6e476d9ca39e3d
jrivals <jerome.rivals@gmail.com>
2018-11-30 19:13:39 +0100

    Ajout d'un service de récupération des villes via l'API adresse.data.gouv

--- src/app/services/municipality-data.service.spec.ts
+++ src/app/services/municipality-data.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MunicipalityDataService } from './municipality-data.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MunicipalityDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: MunicipalityDataService = TestBed.get(MunicipalityDataService);
