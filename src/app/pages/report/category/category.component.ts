fd5a74a8db28584ae1f28d4c45daae1f3cfab934
jrivals <jerome.rivals@gmail.com>
2019-02-06 15:59:37 +0100

    Refonte de l'enchainement des étapes de saisie d'un signalement pour permettre un fonctionnement standard des boutons de navigations du navigateur

---
+++ src/app/pages/report/category/category.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { ReportService, Step } from '../../../services/report.service';
+import { AnalyticsService, EventCategories, ReportEventActions } from '../../../services/analytics.service';
+import { Anomaly } from '../../../model/Anomaly';
+import { Report } from '../../../model/Report';
+import { AnomalyService } from '../../../services/anomaly.service';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.scss']
+})
+export class CategoryComponent implements OnInit {
+
+  step: Step;
+  report: Report;
+
+  anomalies: Anomaly[];
+  showSecondaryCategories: boolean;
+
+  constructor(private anomalyService: AnomalyService,
+              private reportService: ReportService,
+              private analyticsService: AnalyticsService) { }
+
+  ngOnInit() {
+    this.step = Step.Category;
+    this.reportService.currentReport.subscribe(report => this.report = report);
+
+    this.showSecondaryCategories = false;
+    this.loadAnomalies();
+  }
+
+  loadAnomalies() {
+    this.anomalies = this.anomalyService.getAnomalies();
+  }
+
+  primaryCategoriesOrderByRank() {
+    if (this.anomalies) {
+      return this.anomalies
+        .filter(a => a.rank < 100)
+        .sort((a1, a2) => a1.rank > a2.rank ? 1 : a1.rank === a2.rank ? 0 : -1);
+    }
+  }
+
+  secondaryCategoriesOrderByRank() {
+    if (this.anomalies) {
+      return this.anomalies
+        .filter(a => a.rank >= 100)
+        .sort((a1, a2) => a1.rank > a2.rank ? 1 : a1.rank === a2.rank ? 0 : -1);
+    }
+  }
+
+  displaySecondaryCategories() {
+    this.analyticsService.trackEvent(EventCategories.report, ReportEventActions.secondaryCategories);
+    this.showSecondaryCategories = true;
+  }
+
+  selectAnomaly(anomaly: Anomaly) {
+    this.analyticsService.trackEvent(EventCategories.report, ReportEventActions.validateCategory, anomaly.category);
+    this.report = new Report();
+    this.report.category = anomaly.category;
+    this.reportService.changeReport(this.report, this.step);
+  }
+
+}
