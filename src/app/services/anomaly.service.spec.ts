86168bf97d9b760c80ab4559dc3af1438d74ac25
jrivals <jerome.rivals@gmail.com>
2019-02-08 16:03:25 +0100

    Réintégration TU pour test

---
+++ src/app/services/anomaly.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AnomalyService } from './anomaly.service';
+
+describe('AnomalyService', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: []
+    });
+  });
+
+  it('should be created', () => {
+    const service: AnomalyService = TestBed.get(AnomalyService);
+    expect(service).toBeTruthy();
+  });
+});
