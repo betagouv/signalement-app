9bf9ed94d8976b58586087583e07f9bd7030d657
fcoufour <github@coufourier.com>
2019-02-18 18:26:57 +0100

    Configuration d'un service worker

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -2,6 +2,8 @@ import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -10,6 +12,7 @@ import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-bro
     BrowserModule.withServerTransition({ appId: 'app-root' }),
     BrowserTransferStateModule,
     AppModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
   ],
 })
 export class AppBrowserModule {
