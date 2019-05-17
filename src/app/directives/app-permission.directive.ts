2044b061baf17867544b575d1ebc97580c97ffda
jrivals <jerome.rivals@gmail.com>
2019-05-17 14:51:11 +0200

    Affichage des signalements pour le profil DGCCRF

---
+++ src/app/directives/app-permission.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Permissions, User } from '../model/AuthUser';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Directive({
+  selector: '[appPermission]',
+})
+export class AppPermissionDirective implements OnInit {
+
+  @Input() appPermission: Permissions;
+  @Input() appPermissionElse: TemplateRef<any>;
+
+  constructor(private authenticationService: AuthenticationService,
+              private viewContainer: ViewContainerRef,
+              private templateRef: TemplateRef<any>) { }
+
+
+  ngOnInit() {
+    this.authenticationService.user.subscribe(
+      user => {
+        if (this.hasPermission(user, this.appPermission)) {
+          this.viewContainer.createEmbeddedView(this.templateRef);
+        } else if (this.appPermissionElse) {
+          this.viewContainer.createEmbeddedView(this.appPermissionElse);
+        } else {
+          this.templateRef = null;
+        }
+      }
+    );
+  }
+
+  hasPermission(user: User, permission: Permissions) {
+    return user && user.permissions && user.permissions.includes(permission);
+  }
+}
+
