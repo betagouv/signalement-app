c1f3ee476490081765faa902abc2573d706e92e6
pom421 <pierre-olivier.mauguet@beta.gouv.fr>
2019-05-20 17:15:00 +0200

    Ajout Angulartics2RouterlessModule suite à test KO

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Angulartics2RouterlessModule } from 'angulartics2/routerlessmodule';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -19,6 +21,7 @@ describe('LoginComponent', () => {
         ReactiveFormsModule,
         HttpClientModule,
         RouterTestingModule,
+        Angulartics2RouterlessModule.forRoot()
       ]
     })
     .compileComponents();
