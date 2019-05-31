299595bce68adbfa53b5e1fdc01ac5c9366aaa3e
pom421 <pierre-olivier.mauguet@beta.gouv.fr>
2019-05-31 14:51:13 +0200

    Refactor min-width th

--- src/app/pages/secured/reports/list/report-list.component.ts
+++ src/app/pages/secured/reports/list/report-list.component.ts
@@ -256,7 +256,7 @@ export class ReportListComponent implements OnInit, OnDestroy {
   }
 
   getDetailContent(detailInputValues: DetailInputValue[]) {
-    const MAX_CHAR_DETAILS = 56;
+    const MAX_CHAR_DETAILS = 50;
 
     function getLines(str: String, maxLength: Number) {
       function helper(_strings, currentLine, _nbWords) {
