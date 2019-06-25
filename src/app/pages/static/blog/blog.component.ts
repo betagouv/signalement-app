02976aeffd0fe503e97e6b03a9138f69db42bd31
jrivals <jerome.rivals@gmail.com>
2019-06-25 19:32:08 +0200

    Ajout style pour taille des images des articles de blog

--- src/app/pages/static/blog/blog.component.ts
+++ src/app/pages/static/blog/blog.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
 
