3d1f66a50cf1a441c30320f7b77a1bd4bd780c41
jrivals <jerome.rivals@gmail.com>
2019-07-09 14:37:10 +0200

    Correction fichier de conf environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiReportBaseUrl: '',
+  apiReportBaseUrl: '${API_BASE_URL}',
   apiCompanyBaseUrl: 'https://entreprise.data.gouv.fr',
   apiAddressBaseUrl: 'https://api-adresse.data.gouv.fr',
 };
