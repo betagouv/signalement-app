06c13eeef29a68e13997447290eb642edff462d6
pom421 <pierre-olivier.mauguet@beta.gouv.fr>
2019-09-03 16:44:01 +0200

    Ajout pré focus sur titre de la page de remerciement

--- src/app/pages/report/acknowledgment/acknowledgment.component.ts
+++ src/app/pages/report/acknowledgment/acknowledgment.component.ts
@@ -31,6 +31,16 @@ export class AcknowledgmentComponent implements OnInit, OnDestroy {
           this.reportRouterService.routeToFirstStep();
         }
       });
+
+      setTimeout(() => {
+        const title: HTMLElement = document.querySelector("#title-thanks");
+
+        if (title) {
+          title.focus();
+          title.blur();
+        }
+
+      });
   }
 
   ngOnDestroy() {
