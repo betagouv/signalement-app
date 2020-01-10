18782faeb34f27ea7196a4038b1c52022eccbce8
jrivals <jerome.rivals@gmail.com>
2020-01-10 14:49:28 +0100

    Workaround to fix an issue with dynamic routes declaration

--- src/app/pages/report/report.module.ts
+++ src/app/pages/report/report.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { Compiler, FactoryProvider, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, ROUTES, Routes } from '@angular/router';
 import { CategoryComponent, IllustrationCardComponent } from './category/category.component';
 import { InformationComponent } from './information/information.component';
 import { ProblemComponent } from './problem/problem.component';
@@ -24,31 +24,42 @@ import { ReportPaths } from '../../services/report-router.service';
 
 defineLocale('fr', frLocale);
 
-
 const routes: Routes = [{ path: '', component: CategoryComponent }];
-routes.push(
-  ...anomalies.list
-    .map(anomaly => {
-      if (anomaly.information) {
-        return [
-          { path: `${anomaly.path}`, component: InformationComponent },
-          { path: `${anomaly.path}/${ReportPaths.Information}`, component: InformationComponent }
-        ];
-      } else {
-        return [
-          { path: `${anomaly.path}`, component: ProblemComponent },
-          { path: `${anomaly.path}/${ReportPaths.Information}`, component: InformationComponent },
-          { path: `${anomaly.path}/${ReportPaths.Problem}`, component: ProblemComponent },
-          { path: `${anomaly.path}/${ReportPaths.Details}`, component: DetailsComponent },
-          { path: `${anomaly.path}/${ReportPaths.Company}`, component: CompanyComponent },
-          { path: `${anomaly.path}/${ReportPaths.Consumer}`, component: ConsumerComponent },
-          { path: `${anomaly.path}/${ReportPaths.Confirmation}`, component: ConfirmationComponent },
-          { path: `${anomaly.path}/${ReportPaths.Acknowledgment}`, component: AcknowledgmentComponent }
-        ];
-      }
-    })
-    .reduce((routes1, routes2) => [...routes1, ...routes2], [])
-);
+
+// Workaround to fix an issue with dynamic routes declaration.
+// It seems to be fixed with Ivy  rendering engine (angular 9) https://github.com/angular/angular/issues/22700
+export function AnomalyLazyRoutesFactory(compiler: Compiler): Routes {
+  return anomalies.list
+      .map(anomaly => {
+        if (anomaly.information) {
+          return [
+            { path: `${anomaly.path}`, component: InformationComponent },
+            { path: `${anomaly.path}/${ReportPaths.Information}`, component: InformationComponent }
+          ];
+        } else {
+          return [
+            { path: `${anomaly.path}`, component: ProblemComponent },
+            { path: `${anomaly.path}/${ReportPaths.Information}`, component: InformationComponent },
+            { path: `${anomaly.path}/${ReportPaths.Problem}`, component: ProblemComponent },
+            { path: `${anomaly.path}/${ReportPaths.Details}`, component: DetailsComponent },
+            { path: `${anomaly.path}/${ReportPaths.Company}`, component: CompanyComponent },
+            { path: `${anomaly.path}/${ReportPaths.Consumer}`, component: ConsumerComponent },
+            { path: `${anomaly.path}/${ReportPaths.Confirmation}`, component: ConfirmationComponent },
+            { path: `${anomaly.path}/${ReportPaths.Acknowledgment}`, component: AcknowledgmentComponent }
+          ];
+        }
+      })
+      .reduce((routes1, routes2) => [...routes1, ...routes2], []);
+}
+
+export const AnomalyLazyRoutesFactoryProvider: FactoryProvider = <any>{
+  provide: ROUTES,
+  multi: true,
+  deps: [Compiler],
+  useFactory: AnomalyLazyRoutesFactory,
+  useValue: [],
+};
+delete (<any>AnomalyLazyRoutesFactoryProvider).useValue;
 
 @NgModule({
   declarations: [
@@ -92,6 +103,8 @@ routes.push(
   exports: [
     RouterModule,
     AutofocusDirective,
-  ]
+  ],
+  providers: [AnomalyLazyRoutesFactoryProvider]
 })
 export class ReportModule { }
+
