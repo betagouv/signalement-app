10b2465145a89c9fac8eac00d56d7a11c876a581
Thomas Chaumeny <thomas.chaumeny@beta.gouv.fr>
2020-01-14 16:05:08 +0100

    Unsubscribe from user connection on destroy

--- src/app/pages/secured/companies/mycompanies.component.ts
+++ src/app/pages/secured/companies/mycompanies.component.ts
@@ -6,12 +6,16 @@ import { AuthenticationService } from '../../../services/authentication.service'
 import { CompanyAccessesService } from '../../../services/companyaccesses.service';
 import { UserAccess } from "../../../model/CompanyAccess";
 import { User } from "../../../model/AuthUser.js";
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
   selector: 'app-my-companies',
   templateUrl: './mycompanies.component.html'
 })
 export class MyCompaniesComponent implements OnInit {
+
+  private unsubscribe = new Subject<void>();
 
   constructor(private titleService: Title,
               private meta: Meta,
@@ -25,11 +29,18 @@ export class MyCompaniesComponent implements OnInit {
   ngOnInit() {
     this.titleService.setTitle(pages.secured.myCompanies.title);
     this.meta.updateTag({ name: 'description', content: pages.secured.myCompanies.description });
-    this.authenticationService.user.subscribe(user => {
+    this.authenticationService.user
+    .pipe(takeUntil(this.unsubscribe))
+    .subscribe(user => {
       this.user = user;
       this.refreshAccesses();
     });
   }
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
   refreshAccesses() {
     this.companyAccessesService.myAccesses(this.user).subscribe(
       accesses => {
