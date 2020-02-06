383813540cb11293e155da22611041181bcc7443
Thomas Chaumeny <thomas.chaumeny@beta.gouv.fr>
2020-02-06 17:03:27 +0100

    Add env variable for Sentry DSN

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiReportBaseUrl: '${API_BASE_URL}',
   apiCompanyBaseUrl: 'https://entreprise.data.gouv.fr',
   apiAddressBaseUrl: 'https://api-adresse.data.gouv.fr',
-  sentryDsn: 'https://026fe640a3d5408bac25d56b8aa9647c@sentry.data.gouv.fr/44',
+  sentryDsn: '${SENTRY_DSN}',
 };
