6fc8e9a9d8fd8013ebba178a5f7655b218808e57
jrivals <jerome.rivals@gmail.com>
2020-04-07 16:18:47 +0200

    Fix tests

--- src/app/pages/companies/company-card/company-card.component.spec.ts
+++ src/app/pages/companies/company-card/company-card.component.spec.ts
@@ -3,6 +3,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CompanyCardComponent } from './company-card.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppPermissionDirective } from '../../../directives/app-permission/app-permission.directive';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxLoadingModule } from 'ngx-loading';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap';
 
 describe('CompanyCardComponent', () => {
   let component: CompanyCardComponent;
@@ -15,6 +19,11 @@ describe('CompanyCardComponent', () => {
         AppPermissionDirective,
       ],
       imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        NgxLoadingModule,
+        HttpClientModule,
+        ModalModule.forRoot(),
         RouterTestingModule,
       ]
     })
