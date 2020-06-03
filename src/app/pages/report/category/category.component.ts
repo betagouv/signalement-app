9d38b6aee62fc23dc7724bad1f1f9d9898d5e02f
jrivals <jerome.rivals@gmail.com>
2020-06-03 17:28:25 +0200

    replace png with svg

--- src/app/pages/report/category/category.component.ts
+++ src/app/pages/report/category/category.component.ts
@@ -99,10 +99,10 @@ export class CategoryComponent implements OnInit {
 
 
 export const Illustrations = [
-  { title: 'Vous avez rencontré un problème<br/>avec une entreprise&#160;?', picture: 'illustrations/consumer.png' },
-  { title: 'Faites un signalement<br/>avec SignalConso.', picture: 'illustrations/report.png' },
-  { title: `L'entreprise est prévenue<br/>et peut intervenir.`, picture: 'illustrations/company.png' },
-  { title: 'La répression des fraudes intervient<br/>si c’est nécessaire.', picture: 'illustrations/dgccrf.png' },
+  { title: 'Vous avez rencontré un problème<br/>avec une entreprise&#160;?', picture: 'illustrations/consumer.svg' },
+  { title: 'Faites un signalement<br/>avec SignalConso.', picture: 'illustrations/report.svg' },
+  { title: `L'entreprise est prévenue<br/>et peut intervenir.`, picture: 'illustrations/company.svg' },
+  { title: 'La répression des fraudes intervient<br/>si c’est nécessaire.', picture: 'illustrations/dgccrf.svg' },
 ];
 
 @Component({
