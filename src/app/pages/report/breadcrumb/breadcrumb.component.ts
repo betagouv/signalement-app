1913d4192f07388ecaa6e0214e1249b585edd18a
jrivals <jerome.rivals@gmail.com>
2020-07-07 14:04:18 +0200

    Fix issue with breadcrumb steps when refreshing page

--- src/app/pages/report/breadcrumb/breadcrumb.component.ts
+++ src/app/pages/report/breadcrumb/breadcrumb.component.ts
@@ -1,29 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DraftReport, Step } from '../../../model/Report';
 import { AnomalyService } from '../../../services/anomaly.service';
 import { ReportRouterService } from '../../../services/report-router.service';
-import { Anomaly } from '../../../model/Anomaly';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss']
 })
-export class BreadcrumbComponent implements OnInit {
+export class BreadcrumbComponent {
 
   @Input() draftReport: DraftReport;
   @Input() step: Step;
 
   steps = Step;
 
-  anomaly: Anomaly;
-
   constructor(private reportRouterService: ReportRouterService,
               private anomalyService: AnomalyService) { }
 
-  ngOnInit() {
+  getAnomaly() {
     if (this.draftReport) {
-      this.anomaly = this.anomalyService.getAnomalyByCategory(this.draftReport.category);
+      return this.anomalyService.getAnomalyByCategory(this.draftReport.category);
     }
   }
 
@@ -50,7 +47,8 @@ export class BreadcrumbComponent implements OnInit {
   }
 
   getStepNumber(step: Step) {
-    const initialStep = this.anomaly && this.anomaly.subcategories && this.anomaly.subcategories.length ? 0 : -1;
+    const anomaly = this.getAnomaly();
+    const initialStep = anomaly && anomaly.subcategories && anomaly.subcategories.length ? 0 : -1;
     switch (step) {
       case Step.Problem: {
         return initialStep + 1;
