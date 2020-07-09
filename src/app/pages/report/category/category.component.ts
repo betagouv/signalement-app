bdc4aa0d970c485a0b7a663799a34293ebe6f594
jrivals <jerome.rivals@gmail.com>
2020-07-09 11:44:04 +0200

    Fix issue with illustrations alignment on homepage with Firefox

--- src/app/pages/report/category/category.component.ts
+++ src/app/pages/report/category/category.component.ts
@@ -102,7 +102,7 @@ export const Illustrations = [
   { title: 'Vous avez rencontré un problème<br/>avec une entreprise&#160;?', picture: 'illustrations/consumer.png' },
   { title: 'Faites un signalement<br/>avec SignalConso.', picture: 'illustrations/report.png' },
   { title: `L'entreprise est prévenue<br/>et peut intervenir.`, picture: 'illustrations/company.png' },
-  { title: 'La répression des fraudes intervient<br/>si c’est nécessaire.', picture: 'illustrations/dgccrf.png' },
+  { title: 'La répression des fraudes intervient si c’est nécessaire.', picture: 'illustrations/dgccrf.png' },
 ];
 
 @Component({
