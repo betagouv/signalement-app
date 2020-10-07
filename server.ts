26bfd61a3f9e85c8ee284f3dc0a18d0e55c356f2
Thomas Chaumeny <thomas.chaumeny@beta.gouv.fr>
2020-10-07 09:52:29 +0200

    Allow stats.data.gouv.fr frame

--- server.ts
+++ server.ts
@@ -38,7 +38,7 @@ if (process.env.API_BASE_URL) {
       `default-src 'self' stats.data.gouv.fr sentry.data.gouv.fr entreprise.data.gouv.fr ${process.env.API_BASE_URL} 'unsafe-inline';  \
        script-src 'self' stats.data.gouv.fr sentry.data.gouv.fr entreprise.data.gouv.fr 'sha256-WWHGLj0eoGsKPEGMnTqjS4sH0zDInMRPKN098NNWH4E='; \
        img-src 'self' *.data.gouv.fr data: *.numerique.gouv.fr; \
-       frame-src 'self' *.youtube-nocookie.com;`);
+       frame-src 'self' stats.data.gouv.fr *.youtube-nocookie.com;`);
     return next();
   });
 }
