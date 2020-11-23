638cd15882c4770ab0dc02670e532d688a4a570c
Alexandre Annic <alexandre.annic1@gmail.com>
2020-11-23 09:59:49 +0100

    Add Guillaume Rossmann and Alexand Annic member card

--- src/app/pages/static/about/member.component.ts
+++ src/app/pages/static/about/member.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-member',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div class="team-member-info">
       <div class="team-member-name">{{name}}</div>
       <div class="team-member-role">
-        {{role}}
+        {{role}}<br/>
+        <span *ngIf="!dgccre">&nbsp;</span>
         <ng-container *ngIf="dgccre">
-          <br/>Inspecteur <abbr title="Direction Générale de la Concurrence, Consommation et Répression des Fraudes">DGCCRF</abbr>
+          Inspecteur <abbr title="Direction Générale de la Concurrence, Consommation et Répression des Fraudes">DGCCRF</abbr>
         </ng-container>
       </div>
     </div>
-    <img src="/assets/images/avatars/{{avatar}}" alt=""/>
+    <img src="/assets/images/avatars/{{avatar}}" alt="Avatar {{name}}"/>
   `,
   host: {
     '[class.old]': 'disabled',
