cd08d70dc1cdb47bcb85d8e5ab831f0f5f74ca3f
jrivals <jerome.rivals@gmail.com>
2020-12-23 17:34:48 +0100

    Add test on filtering

--- src/app/dashboard/websites/unregistered/websites-unregistered.component.spec.ts
+++ src/app/dashboard/websites/unregistered/websites-unregistered.component.spec.ts
@@ -7,6 +7,9 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
 import { WebsiteService } from '../../../services/website.service';
+import { addMonths } from 'date-fns';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale, frLocale } from 'ngx-bootstrap/chronos';
 
 describe('UnregisteredComponent', () => {
 
@@ -21,13 +24,15 @@ describe('UnregisteredComponent', () => {
         ComponentsModule,
         SharedModule,
         RouterTestingModule,
-        NoopAnimationsModule
+        NoopAnimationsModule,
+        BsDatepickerModule.forRoot(),
       ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    defineLocale('fr', frLocale);
     websiteService = TestBed.inject(WebsiteService);
     fixture = TestBed.createComponent(WebsitesUnregisteredComponent);
     component = fixture.componentInstance;
@@ -42,6 +47,22 @@ describe('UnregisteredComponent', () => {
     const nativeElement = fixture.nativeElement;
     expect(nativeElement.querySelector('tbody')).not.toBeNull();
     expect(nativeElement.querySelectorAll('tbody > tr').length).toBe(2);
+
+  });
+
+  it('should request the API on filtering', () => {
+
+    const websiteServiceSpy = spyOn(websiteService, 'listUnregistered').and.callFake(() => of([{ host: 'host1.fr', count: 1}, { host: 'host2.fr', count: 2}]));
+
+    const q = 'host';
+    const start = addMonths(new Date(), -1);
+    const end = new Date();
+
+    component.hostFilter = q;
+    component.periodFilter = [start, end];
+    fixture.detectChanges();
+
+    expect(websiteServiceSpy).toHaveBeenCalledWith('host', start, end);
 
   });
 });
