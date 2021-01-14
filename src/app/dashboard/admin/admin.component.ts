e194116deb2dc40f7bc9a0ee57b4341108eb9840
Alexandre Annic <alexandre.annic1@gmail.com>
2021-01-14 16:48:13 +0100

    Fix infinite loading on form

--- src/app/dashboard/admin/admin.component.ts
+++ src/app/dashboard/admin/admin.component.ts
@@ -85,6 +85,7 @@ export class AdminComponent implements OnInit {
             this.emailRejectedError = err.error;
           });
     } else {
+      this.loading = false;
       this.showErrors = true;
     }
   }
