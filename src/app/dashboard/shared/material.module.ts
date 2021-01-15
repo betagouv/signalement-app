c29c81bffafba70604d07a6ec0ac27808799691b
Alexandre Annic <alexandre.annic1@gmail.com>
2021-01-15 09:31:50 +0100

    Fix label

--- src/app/dashboard/shared/material.module.ts
+++ src/app/dashboard/shared/material.module.ts
@@ -20,7 +20,19 @@ export class MatPaginatorIntlFr extends MatPaginatorIntl {
   itemsPerPageLabel = 'Lignes par page';
   nextPageLabel = 'Suivant';
   previousPageLabel = 'Précédent';
-  getRangeLabel = (pageIndex: number, pageSize: number, length: number) => `${pageIndex * pageSize} - ${(pageIndex + 1) * pageSize} sur ${length}`;
+  getRangeLabel = (page: number, pageSize: number, length: number) => {
+    if (length === 0 || pageSize === 0) {
+      return `0 sur ${length}`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    // If the start index exceeds the list length, do not try and fix the end index to the end.
+    const endIndex = startIndex < length
+      ? Math.min(startIndex + pageSize, length)
+      : startIndex + pageSize;
+
+    return `${startIndex + 1} – ${endIndex} sur ${length}`;
+  };
 }
 
 @NgModule({
