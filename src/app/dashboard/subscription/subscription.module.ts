7be939b8c75e250e0aab353808af23c3ac32e2dd
Alexandre Annic <alexandre.annic1@gmail.com>
2021-02-18 14:59:32 +0100

    Remove dead files

--- src/app/dashboard/subscription/subscription.module.ts
+++ src/app/dashboard/subscription/subscription.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../../guards/auth.guard';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentsModule } from '../../components/components.module';
-import { SubscriptionDetailComponent, SubscriptionDialogDirective } from './subscription-detail/subscription-detail.component';
 import { SubscriptionListComponent } from './subscription-list/subscription-list.component';
 import { Roles } from '../../model/AuthUser';
 import { SharedModule } from '../shared/shared.module';
 import { SubscriptionCardComponent } from './subscription-card/subscription-card.component';
 import { SelectDepartmentsModule } from '../reports/list/select-departments/select-departments.module';
 import { PanelModule } from '../shared/panel/panel.module';
-import { SubscriptionListItemComponent } from './list/subscription-list-item.component';
 import { CountryDialogModule } from '../reports/list/country-dialog/country-dialog.module';
 import { MaterialModule } from '../shared/material.module';
 import { SubscriptionCardRowComponent } from './subscription-card/subscription-card-row.component';
@@ -20,18 +18,13 @@ import { CompanySearchDialogModule } from '../companies/company-search-dialog/co
 
 const routes: Routes = [
   { path: 'abonnements', component: SubscriptionListComponent, canActivate: [AuthGuard], data: { expectedRoles: [Roles.Admin, Roles.DGCCRF] } },
-  { path: 'abonnements/nouveau', component: SubscriptionDetailComponent, canActivate: [AuthGuard], data: { expectedRoles: [Roles.Admin, Roles.DGCCRF] } },
-  { path: 'abonnements/:subscriptionId', component: SubscriptionDetailComponent, canActivate: [AuthGuard], data: { expectedRoles: [Roles.Admin, Roles.DGCCRF] } },
 ];
 
 @NgModule({
   declarations: [
     SelectDialogComponent,
     SelectDepartmentsDialogComponent,
-    SubscriptionDialogDirective,
-    SubscriptionDetailComponent,
     SubscriptionListComponent,
-    SubscriptionListItemComponent,
     SubscriptionCardComponent,
     SubscriptionCardRowComponent,
   ],
