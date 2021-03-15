480c53cbc19af0720c3b57ca1b9798351438546f
Alexandre Annic <alexandre.annic1@gmail.com>
2021-03-15 14:33:24 +0100

    Fix scrolling issue

--- src/app/dashboard/reports/list/select-departments/select-departments.component.ts
+++ src/app/dashboard/reports/list/select-departments/select-departments.component.ts
@@ -1,35 +1,37 @@
 import { Component, forwardRef, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import Utils from '../../../../utils';
-import { Region, Regions } from '../../../../model/Region';
+import { Department, Region, Regions } from '../../../../model/Region';
 import { MatPseudoCheckboxState } from '@angular/material/core';
+import { slideToggleNgIf } from '../../../../utils/animations';
 
 @Component({
   selector: 'app-select-departments',
   template: `
-    <mat-select [placeholder]="placeholder" multiple [(ngModel)]="value" (ngModelChange)="onChangeCallback(this.value)"
-                [disabled]="disabled">
+    <mat-select [placeholder]="placeholder" multiple [disabled]="disabled">
       <mat-option class="mat-option-dense" [hidden]="true">Hack because panel won't open is there is not mat-option</mat-option>
       <div class="mat-option select-optgroup" matRipple (click)="toggleAllDepartments()">
-        <mat-pseudo-checkbox [state]="getDepartmentsCheckboxState()" class="mat-option-pseudo-checkbox"></mat-pseudo-checkbox>
+        <mat-pseudo-checkbox [state]="getRegionCheckboxState()" class="mat-option-pseudo-checkbox"></mat-pseudo-checkbox>
         Tous les départements
       </div>
       <ng-container *ngFor="let region of regions">
         <div class="mat-option select-optgroup" matRipple (click)="toggleAllDepartments(region)">
-          <mat-pseudo-checkbox class="mat-option-pseudo-checkbox" [state]="getDepartmentsCheckboxState(region)"></mat-pseudo-checkbox>
+          <mat-pseudo-checkbox class="mat-option-pseudo-checkbox" [state]="getRegionCheckboxState(region)"></mat-pseudo-checkbox>
           {{region.label}}
           <mat-icon class="-expend_more" (click)="toggleVisibility(region.label, $event)">
             {{visibleRegions.has(region.label) ? 'expand_more' : 'navigate_next'}}
           </mat-icon>
         </div>
-        <ng-container *ngIf="visibleRegions.has(region.label)">
-          <mat-option class="mat-option-dense txt-secondary" *ngFor="let dep of region.departments" [value]="dep.code">
+        <div *ngIf="visibleRegions.has(region.label)" @slideToggleNgIf>
+          <div class="mat-option mat-option-dense txt-secondary" *ngFor="let dep of region.departments" (click)="toggleDepartment(dep)"
+               matRipple>
+            <mat-pseudo-checkbox class="mat-option-pseudo-checkbox" [state]="getDepartmentCheckboxState(dep)"></mat-pseudo-checkbox>
             ({{dep.code}}) {{dep.label}}
-          </mat-option>
-        </ng-container>
+          </div>
+        </div>
       </ng-container>
     </mat-select>
   `,
+  animations: [slideToggleNgIf],
   styleUrls: ['./select-departments.component.scss'],
   providers: [{
     provide: NG_VALUE_ACCESSOR,
@@ -38,9 +40,6 @@ import { MatPseudoCheckboxState } from '@angular/material/core';
   }],
 })
 export class SelectDepartmentsComponent implements ControlValueAccessor {
-
-  constructor() {
-  }
 
   @Input() disabled?: boolean;
 
@@ -52,12 +51,69 @@ export class SelectDepartmentsComponent implements ControlValueAccessor {
 
   readonly departments = this.regions.flatMap(_ => _.departments).map(_ => _.code);
 
-  value: string[];
+  selectedDepartments = new Set<string>();
 
-  writeValue(value: any): void {
-    this.value = value;
-    this.onChangeCallback(this.value);
+  writeValue(value: string[]): void {
+    this.selectedDepartments = new Set(value);
+    this.onChangeCallback(value);
   }
+
+  readonly departmentCodes = (r?: Region): string[] => r ? r.departments.map(_ => _.code) : this.departments;
+
+  private readonly allDepartmentsSelected = (r?: Region): boolean => {
+    return this.departmentCodes(r).every(_ => this.selectedDepartments.has(_));
+  };
+
+  private readonly noDepartmentsSelected = (r?: Region): boolean => {
+    return !this.departmentCodes(r).find(_ => this.selectedDepartments.has(_));
+  };
+
+  readonly getRegionCheckboxState = (r?: Region): MatPseudoCheckboxState => {
+    if (this.noDepartmentsSelected(r)) {
+      return 'unchecked';
+    }
+    if (this.allDepartmentsSelected(r)) {
+      return 'checked';
+    }
+    return 'indeterminate';
+  };
+
+  readonly getDepartmentCheckboxState = (d: Department): MatPseudoCheckboxState => this.selectedDepartments.has(d.code) ? 'checked' : 'unchecked';
+
+  readonly toggleAllDepartments = (region?: Region) => {
+    if (this.noDepartmentsSelected(region)) {
+      this.selectAllDepartments(region);
+    } else {
+      this.removeAllDepartments(region);
+    }
+    this.onChangeCallback([...this.selectedDepartments]);
+  };
+
+  private selectAllDepartments = (r?: Region) => {
+    this.departmentCodes(r).forEach(_ => this.selectedDepartments.add(_));
+  };
+
+  private removeAllDepartments = (r?: Region) => {
+    this.departmentCodes(r).forEach(_ => this.selectedDepartments.delete(_));
+  };
+
+  readonly toggleDepartment = (dep: Department) => {
+    if (this.selectedDepartments.has(dep.code)) {
+      this.selectedDepartments.delete(dep.code);
+    } else {
+      this.selectedDepartments.add(dep.code);
+    }
+    this.onChangeCallback([...this.selectedDepartments]);
+  };
+
+  readonly toggleVisibility = (r: string, $event: any) => {
+    $event.stopPropagation();
+    if (this.visibleRegions.has(r)) {
+      this.visibleRegions.delete(r);
+    } else {
+      this.visibleRegions.add(r);
+    }
+  };
 
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
@@ -76,56 +132,4 @@ export class SelectDepartmentsComponent implements ControlValueAccessor {
   setDisabledState(isDisabled: boolean): void {
     this.disabled = isDisabled;
   }
-
-  private allDepartmentsSelected = (r?: Region): boolean => {
-    const departments = r ? r.departments.map(_ => _.code) : this.departments;
-    return departments.every(_ => this.safeDepartmentInputValue.includes(_));
-  };
-
-  private noDepartmentsSelected = (r?: Region): boolean => {
-    const departments = r ? r.departments.map(_ => _.code) : this.departments;
-    return !departments.find(_ => this.safeDepartmentInputValue.includes(_));
-  };
-
-  getDepartmentsCheckboxState = (r?: Region): MatPseudoCheckboxState => {
-    if (this.noDepartmentsSelected(r)) {
-      return 'unchecked';
-    }
-    if (this.allDepartmentsSelected(r)) {
-      return 'checked';
-    }
-    return 'indeterminate';
-  };
-
-  toggleAllDepartments = (region?: Region) => {
-    if (this.noDepartmentsSelected(region)) {
-      this.selectAllDepartments(region);
-    } else {
-      this.removeAllDepartments(region);
-    }
-    this.onChangeCallback(this.value);
-  };
-
-  get safeDepartmentInputValue() {
-    return this.value || [];
-  }
-
-  private selectAllDepartments = (region?: Region) => {
-    const departments = region ? region.departments.map(_ => _.code) : this.departments;
-    this.value = Utils.uniqueValues([...this.safeDepartmentInputValue, ...departments]);
-  };
-
-  private removeAllDepartments = (region?: Region) => {
-    const departments = region ? region.departments.map(_ => _.code) : this.departments;
-    this.value = this.safeDepartmentInputValue.filter(_ => !departments.includes(_));
-  };
-
-  readonly toggleVisibility = (region: string, $event: any) => {
-    $event.stopPropagation();
-    if (this.visibleRegions.has(region)) {
-      this.visibleRegions.delete(region);
-    } else {
-      this.visibleRegions.add(region);
-    }
-  };
 }
