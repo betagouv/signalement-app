ddf8ae9364d1d9a08c0081c51bd28490e882db32
Alexandre Annic <alexandre.annic1@gmail.com>
2021-03-24 17:25:34 +0100

    Fix

--- server.ts
+++ server.ts
@@ -39,8 +39,8 @@ export function app() {
 
   if (process.env.API_BASE_URL) {
     server.use(function(req, res, next) {
-      res.setHeader('Content-Security-Policy', `
-        default-src 'self' stats.data.gouv.fr sentry.data.gouv.fr entreprise.data.gouv.fr ${process.env.API_BASE_URL} 'unsafe-inline';  \
+      res.setHeader('Content-Security-Policy',
+        `default-src 'self' stats.data.gouv.fr sentry.data.gouv.fr entreprise.data.gouv.fr ${process.env.API_BASE_URL} 'unsafe-inline';  \
         script-src 'self' stats.data.gouv.fr sentry.data.gouv.fr tag.aticdn.net entreprise.data.gouv.fr 'sha256-WWHGLj0eoGsKPEGMnTqjS4sH0zDInMRPKN098NNWH4E='; \
         img-src 'self' *.data.gouv.fr data: *.numerique.gouv.fr *.xiti.com; \
         frame-src 'self' stats.data.gouv.fr *.youtube-nocookie.com; \
