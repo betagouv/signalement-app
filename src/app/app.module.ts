ce63cf8aa35ebe306daf92574203a1ba9a78b138
Alexandre Annic <alexandre.annic1@gmail.com>
2021-04-05 09:54:44 +0200

    Try another way to init Sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgtUniversalModule } from '@ng-toolkit/universal';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import { TransferHttpCacheModule } from '@nguniversal/common';
-import { ErrorHandler, LOCALE_ID, NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './pages/footer/footer.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ReportModule } from './pages/report/report.module';
 import localeFr from '@angular/common/locales/fr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -17,36 +17,17 @@ import { AccountModule } from './pages/account/account.module';
 import { Angulartics2Module } from 'angulartics2';
 import { ComponentsModule, NgxLoadingConfig } from './components/components.module';
 import { environment } from '../environments/environment';
-import * as SentryBrowser from '@sentry/browser';
 import { AppRoleModule } from './directives/app-role/app-role.module';
 import { AppPermissionModule } from './directives/app-permission/app-permission.module';
 import { HeaderModule } from './pages/header/header.module';
 import { ContractualDisputeModule } from './pages/contractual-dispute/contractual-dispute.module';
 import { NgxLoadingModule } from 'ngx-loading';
 import { CompaniesModule } from './pages/companies/companies.module';
+import * as Sentry from '@sentry/angular';
 
 registerLocaleData(localeFr, 'fr');
 
-class ErrorLogger extends ErrorHandler {
-
-  static initWith(sentry: any) {
-    return () => new ErrorLogger(sentry);
-  }
-
-  constructor(private sentry: any) {
-    super();
-    if (environment.sentryDsn) {
-      this.sentry.init({ dsn: environment.sentryDsn });
-    }
-  }
-
-  handleError(error: any): void {
-    if (environment.sentryDsn) {
-      this.sentry.captureException(error.originalError || error);
-    }
-    super.handleError(error);   // for default behaviour rather than silently dying
-  }
-}
+Sentry.init({ dsn: environment.sentryDsn });
 
 @NgModule({
     declarations: [
@@ -89,8 +70,10 @@ class ErrorLogger extends ErrorHandler {
     exports: [
     ],
     providers: [
-        { provide: LOCALE_ID, useValue: 'fr' },
-        { provide: ErrorHandler, useFactory: ErrorLogger.initWith(SentryBrowser) },
+      { provide: LOCALE_ID, useValue: 'fr' },
+      { provide: ErrorHandler, useValue: Sentry.createErrorHandler()},
+      { provide: Sentry.TraceService, deps: [Router], },
+      { provide: APP_INITIALIZER, useFactory: () => () => {}, deps: [Sentry.TraceService], multi: true, },
     ],
 })
 export class AppModule {
