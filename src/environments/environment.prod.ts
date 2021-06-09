66f871f21494306460c17327816a6b2c3fc0b3c0
Alexandre Annic <alexandre.annic1@gmail.com>
2021-06-09 16:51:33 +0200

    Allow to override reponseconso url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,12 @@ export const environment = {
   apiReportBaseUrl: '${API_BASE_URL}',
   apiCompanyBaseUrl: 'https://entreprise.data.gouv.fr',
   apiAddressBaseUrl: 'https://api-adresse.data.gouv.fr',
-  reponseConsoDisplayRate: Number('${REPONSECONSO_DISPLAY_PERCENTAGE}') || 0,
-  reponseConsoForwardUrl: (id: string) => `https://reclamation.conso.gouv.fr/${id}`,
+  reponseConsoDisplayRate: Number('${REPONSECONSO_DISPLAY_PERCENTAGE}') || 0,
+  reponseConsoForwardUrl: (id: string) => {
+    const envUrl = '${REPONSECONSO_BASE_URL}';
+    // @ts-ignore
+    const baseUrl = (!envUrl || envUrl === '') ? 'https://reclamation.conso.gouv.fr' : envUrl.replace('\/$', '');
+    return `${baseUrl}/${id}`;
+  },
   sentryDsn: '${SENTRY_DSN}',
 };
