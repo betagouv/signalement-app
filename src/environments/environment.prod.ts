b11e9e0bdf06cb482fcf62fb8fe6af7a2750a12b
Said SEDOUD <said.sedoud@ingenico.com>
2021-08-03 16:25:35 +0200

    Fix bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  apiReportBaseUrl: '${API_BASE_URL}',
+  apiReportBaseUrl: '',
   apiCompanyBaseUrl: 'https://entreprise.data.gouv.fr',
   apiAddressBaseUrl: 'https://api-adresse.data.gouv.fr',
-  reponseConsoDisplayRate: Number('${REPONSECONSO_DISPLAY_PERCENTAGE}') || 0,
+  reponseConsoDisplayRate: Number('') || 0,
   reponseConsoForwardUrl: (id: string) => {
-    const envUrl = '${REPONSECONSO_BASE_URL}';
+    const envUrl = '';
     // @ts-ignore
     const baseUrl = (!envUrl || envUrl === '') ? 'https://reclamation.conso.gouv.fr' : envUrl.replace('\/$', '');
     return `${baseUrl}/${id}`;
   },
-  sentryDsn: '${SENTRY_DSN}',
+  sentryDsn: '',
 };
