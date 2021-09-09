322ba4b58da003c53f571b519cb0fde9c6e7fcaa
Alexandre Annic <alexandre.annic1@gmail.com>
2021-09-09 12:05:05 +0200

    Restore environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,15 @@
 export const environment = {
   production: true,
-  apiReportBaseUrl: 'http://localhost:9000',
+  dashboardBaseUrl: '${DASHBOARD_BASE_URL}',
+  apiReportBaseUrl: '${API_BASE_URL}',
   apiCompanyBaseUrl: 'https://entreprise.data.gouv.fr',
   apiAddressBaseUrl: 'https://api-adresse.data.gouv.fr',
-  reponseConsoDisplayRate: Number('') || 0,
+  reponseConsoDisplayRate: Number('${REPONSECONSO_DISPLAY_PERCENTAGE}') || 0,
   reponseConsoForwardUrl: (id: string) => {
-    const envUrl = '';
+    const envUrl = '${REPONSECONSO_BASE_URL}';
     // @ts-ignore
     const baseUrl = (!envUrl || envUrl === '') ? 'https://reclamation.conso.gouv.fr' : envUrl.replace('\/$', '');
     return `${baseUrl}/${id}`;
   },
-  sentryDsn: '',
+  sentryDsn: '${SENTRY_DSN}',
 };
