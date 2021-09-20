9bdd4572a3fc8c4c8b0aa9e4e6da27da5c9f2375
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2021-09-20 15:48:42 +0200

    adding email validation switch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   dashboardBaseUrl: '${DASHBOARD_BASE_URL}',
   apiReportBaseUrl: '${API_BASE_URL}',
-  skipReportEmailValidation: ('${API_BASE_URL}' as string | undefined) === 'true',
+  skipReportEmailValidation: ('${SKIP_REPORT_EMAIL_VALIDATION}' as string | undefined) === 'true',
   apiReportBaseUrlX: ('${API_BASE_URL}' as string | undefined) === 'true',
   apiCompanyBaseUrl: 'https://entreprise.data.gouv.fr',
   apiAddressBaseUrl: 'https://api-adresse.data.gouv.fr',
