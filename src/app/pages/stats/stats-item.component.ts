d3b1c05feb6919071efa5c8783cf8b403526a816
Alexandre Annic <alexandre.annic1@gmail.com>
2021-10-11 15:11:05 +0200

    Clean

--- src/app/pages/stats/stats-item.component.ts
+++ src/app/pages/stats/stats-item.component.ts
@@ -4,12 +4,6 @@ import { mergeMap, tap } from 'rxjs/operators';
 import { isPlatformBrowser } from '@angular/common';
 import { Observable } from 'rxjs';
 
-interface ObsHandler<T> {
-  value?: T;
-  loading?: boolean;
-  load: () => void;
-}
-
 @Component({
   selector: 'app-stats-item',
   template: `
@@ -32,10 +26,7 @@ export class StatsItemComponent implements OnInit {
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
-    public cdr: ChangeDetectorRef
   ) {
-    // this.value = this.handleObservable(() => this.value$);
-    // this.chart = this.handleObservable(() => this.chart$);
   }
 
   @Input() title: string;
@@ -46,33 +37,7 @@ export class StatsItemComponent implements OnInit {
 
   @Input() chart$: Observable<EChartOption[]>;
 
-  // value: ObsHandler<string>;
-  //
-  // chart: ObsHandler<EChartOption[]>;
-
-  readonly handleObservable = <T>(obs: () => Observable<T>): ObsHandler<T> => {
-    let loading = false;
-    let value: T | undefined;
-    const mappedObs = new Observable(_ => _.next()).pipe(
-      tap(_ => {
-        loading = true;
-      }),
-      mergeMap(obs),
-      tap(_ => {
-        value = _;
-        loading = false;
-        this.cdr.detectChanges();
-      })
-    );
-    return {
-      loading,
-      value,
-      load: () => mappedObs.subscribe()
-    };
-  };
-
   ngOnInit() {
-  //   this.value.load();
   }
 
   readonly renderCharts = () => isPlatformBrowser(this.platformId);
