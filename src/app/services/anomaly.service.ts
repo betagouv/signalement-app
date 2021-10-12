a2e94cc19b274eff7fca492530993de62b1c43ff
Alexandre Annic <alexandre.annic1@gmail.com>
2021-10-12 13:58:01 +0200

    Remove dead code

--- src/app/services/anomaly.service.ts
+++ src/app/services/anomaly.service.ts
@@ -29,14 +29,4 @@ export class AnomalyService {
   getAnomalyByCategoryId(categoryId: string) {
     return this.getAnomalyBy(anomaly => anomaly.categoryId === categoryId);
   }
-
-  getCategories() {
-    return this.anomalies.filter(anomaly => !anomaly.information).map(anomaly => anomaly.category);
-  }
-
-  getTags(): string[] {
-    return [InternetTag].concat(...this.anomalies.map(anomaly => collectTags(anomaly)))
-      .filter((tag, index, tags) => tags.indexOf(tag) === index)
-      .sort((t1, t2) => t1.toString().localeCompare(t2.toString()));
-  }
 }
