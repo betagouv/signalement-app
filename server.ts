7b27bdcfd41fb38da287a02aba7c7f92aa5de822
Alexandre Annic <alexandre.annic1@gmail.com>
2021-10-15 15:13:20 +0200

    Try to fix Matomo

--- server.ts
+++ server.ts
@@ -40,7 +40,7 @@ export function app() {
     server.use(function(req, res, next) {
       res.setHeader('Content-Security-Policy',
         `default-src 'self' stats.data.gouv.fr entreprise.data.gouv.fr ${process.env.API_BASE_URL} *.ingest.sentry.io 'unsafe-inline';  \
-        script-src 'self' 'unsafe-inline' stats.data.gouv.fr *.ingest.sentry.io tag.aticdn.net entreprise.data.gouv.fr 'sha256-WWHGLj0eoGsKPEGMnTqjS4sH0zDInMRPKN098NNWH4E='; \
+        script-src 'self' stats.data.gouv.fr *.ingest.sentry.io tag.aticdn.net entreprise.data.gouv.fr 'sha256-ALiClHzUO8TDq0Xaef0oCY77GxCNVrH2t+9awalvG+o='; \
         img-src 'self' *.data.gouv.fr data: *.numerique.gouv.fr *.xiti.com; \
         frame-src 'self' stats.data.gouv.fr *.youtube-nocookie.com; \
         style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; \
