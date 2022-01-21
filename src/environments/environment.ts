4c37a37c4431161e663e8c3e4d2b1c2d9d072021
Alexandre Annic <alexandre.annic1@gmail.com>
2022-01-21 10:32:35 +0100

    Stop forwarding UUID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,6 @@ export const environment = {
   apiCompanyBaseUrl: 'https://entreprise.data.gouv.fr',
   apiAddressBaseUrl: 'https://api-adresse.data.gouv.fr',
   reponseConsoDisplayRate: 100,
-  reponseConsoForwardUrl: (id: string) => `https://reclamation-preprod.conso.gouv.fr/${id}`,
   sentryDsn: null,
   consumerEmailBlackList: undefined,
 };
